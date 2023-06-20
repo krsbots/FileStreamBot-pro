607b555250ab2a59aa22d4ce61c7c3b03d525d20
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-06-20 11:53:49 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,23 +9,23 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv("API_ID"))
-    API_HASH = str(getenv("API_HASH"))
-    BOT_TOKEN = str(getenv("BOT_TOKEN"))
+    API_ID = int(getenv("API_ID" "20804756"))
+    API_HASH = str(getenv("API_HASH" "ecd0e2a4cc383ae5717059e7ae120adb"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN" "6136516898:AAHUuHh4mUhAuLEWYnB7NdqMa9JszHMFBJQ"))
     name = str(getenv("name", "mplaylinkstream"))
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "4"))
-    BIN_CHANNEL = int(getenv("BIN_CHANNEL"))
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL" "-1001949163175"))
     PORT = int(getenv("PORT", 8080))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "").split()}
+    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "5576458964").split()}
     NO_PORT = bool(getenv("NO_PORT", False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv("OWNER_USERNAME"))
+    OWNER_USERNAME = str(getenv("OWNER_USERNAME" "MPlaylink_Team"))
     if "DYNO" in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv("APP_NAME"))
+        APP_NAME = str(getenv("APP_NAME" "mplaylin"))
 
     else:
         ON_HEROKU = False
@@ -36,7 +36,7 @@ class Var(object):
     )
     HAS_SSL = bool(getenv("HAS_SSL", False))
     URL = f"https://{FQDN}/" if HAS_SSL else f"http://{FQDN}/"
-    DATABASE_URL = str(getenv("DATABASE_URL"))
+    DATABASE_URL = str(getenv("DATABASE_URL" "mongodb+srv://MdMatin:x7bdggKJ9zb9JSK@cluster0.89bzvjn.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv("UPDATES_CHANNEL", None))
     BANNED_CHANNELS = list(
         {int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()}
