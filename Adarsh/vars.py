25d0967b8f5008fdc2a004f5e5713154c93bbfa9
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-06-21 16:06:41 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,8 +9,8 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv("API_ID", "20804756"))
-    API_HASH = str(getenv("API_HASH" "ecd0e2a4cc383ae5717059e7ae120adb"))
+    API_ID = int(getenv("API_ID", "21407623"))
+    API_HASH = str(getenv("API_HASH" "71526961879d5d662ec9307fe850da90"))
     BOT_TOKEN = str(getenv("BOT_TOKEN" "6136516898:AAFoiy-XM8w7RhhIvLLA6qWOjzIsIsl_h8c"))
     name = str(getenv("name", "m2n3b4v5c6x7z"))
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
@@ -19,10 +19,10 @@ class Var(object):
     PORT = int(getenv("PORT", 8080))
     BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "5576458964").split()}
+    OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "5297391576").split()}
     NO_PORT = bool(getenv("NO_PORT", False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv("OWNER_USERNAME" "MPlaylink_Team"))
+    OWNER_USERNAME = str(getenv("OWNER_USERNAME" "chill_yaaaar"))
     if "DYNO" in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv("APP_NAME" "m2n3b4v5c6x7z"))
