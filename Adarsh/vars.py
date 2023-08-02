9fc6784bbcdca67179fd297bc5d310486ef72b13
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-08-02 08:54:03 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -25,7 +25,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv("OWNER_USERNAME", "chill_yaaaar"))
     if "DYNO" in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv("APP_NAME", "mplaystreamstream"))
+        APP_NAME = str(getenv("APP_NAME", "filestreampromplaylink"))
 
     else:
         ON_HEROKU = False
