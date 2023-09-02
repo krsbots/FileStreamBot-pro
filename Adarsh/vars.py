f57746c71f6d3e8b7934eefc7c1594af9054a8cd
krsbots <143610563+krsbots@users.noreply.github.com>
2023-09-03 01:31:11 +0600

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,10 +9,10 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv("API_ID", "21407623"))
-    API_HASH = str(getenv("API_HASH", "71526961879d5d662ec9307fe850da90"))
+    API_ID = int(getenv("API_ID", "25092262"))
+    API_HASH = str(getenv("API_HASH", "6154a0b7f5c3e8605640d3d5f8093e78"))
     BOT_TOKEN = str(getenv("BOT_TOKEN","6171238258:AAEYOhFc9lfpZFoIgewvOWeZ4sSBbxBTrzE"))
-    name = str(getenv("name", "File-To-Link"))
+    name = str(getenv("name", "moneycasefilestream"))
     SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
     WORKERS = int(getenv("WORKERS", "4"))
     BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001748699071"))
@@ -32,10 +32,10 @@ class Var(object):
     FQDN = (
         str(getenv("FQDN", BIND_ADRESS))
         if not ON_HEROKU or getenv("FQDN")
-        else f"{APP_NAME}.herokuapp.com"
+        else f"vivdisk.pro"
     )
     HAS_SSL = bool(getenv("HAS_SSL", False))
-    URL = f"https://{FQDN}/" if HAS_SSL else f"http://{FQDN}/"
+    URL = f"https://vivdisk.pro/" if HAS_SSL else f"http://vivdisk.pro/"
     DATABASE_URL = str(getenv("DATABASE_URL", "mongodb+srv://MdMatin:x7bdggKJ9zb9JSK@cluster0.89bzvjn.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv("UPDATES_CHANNEL", None))
     BANNED_CHANNELS = list(
